ef4d3c520606787e0d2a93e3d48e0fd5b9140f88
lixianSharp <861034458@qq.com>
2018-09-18 15:32:38 +0800

    完成了有道云模块的后台开发

--- lixianyuan/src/main/java/lixianyuan/service/ydyblog/impl/YdyBlogServiceImpl.java
+++ lixianyuan/src/main/java/lixianyuan/service/ydyblog/impl/YdyBlogServiceImpl.java
@@ -1,18 +1,24 @@
 package lixianyuan.service.ydyblog.impl;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lixianyuan.bean.Ydyblog;
 import lixianyuan.mapper.YdyblogMapper;
+import lixianyuan.mapper.custom.YdyblogCustomMapper;
 import lixianyuan.service.ydyblog.YdyBlogService;
 @Service
 public class YdyBlogServiceImpl implements YdyBlogService{
 	
 	@Autowired
 	private YdyblogMapper ydyblogMapper;
+	
+	//自定义的mapper
+	@Autowired
+	private YdyblogCustomMapper ydyblogCustomMapper;
 
 	//增加
 	public int insertYdyblog(Ydyblog ydyblog) {
@@ -32,10 +38,16 @@ public class YdyBlogServiceImpl implements YdyBlogService{
 		return result;
 	}
 
-	//查询出所有，分页查询
-	public List<Ydyblog> findAllYdyblog() {
-		// TODO Auto-generated method stub
-		return null;
+	//根据条件查询出符合条件的有道云笔记，分页查询
+	public List<Ydyblog> findAllydyblogByCondigion(Map<String,Object> map) {
+		List<Ydyblog> result = ydyblogCustomMapper.findAllydyblogByCondigion(map);
+		return result.size()>0?result:null;
+	}
+
+	//根据条件查询出总记录数
+	public int findAllydyblogCountByCondigion(Map<String, Object> map) {
+		int result = ydyblogCustomMapper.findAllydyblogCountByCondigion(map);
+		return result>0?result:0;
 	}
 
 }
