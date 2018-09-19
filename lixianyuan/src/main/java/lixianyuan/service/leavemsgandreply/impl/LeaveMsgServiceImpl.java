a6242bfb911b61d37e961a359ec36ef65914cb62
lixianSharp <861034458@qq.com>
2018-09-19 11:42:49 +0800

    留言和回复留言模块的后台部分开完完成

---
+++ lixianyuan/src/main/java/lixianyuan/service/leavemsgandreply/impl/LeaveMsgServiceImpl.java
@@ -0,0 +1,91 @@
+package lixianyuan.service.leavemsgandreply.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.websocket.server.ServerEndpoint;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lixianyuan.bean.Leavemsg;
+import lixianyuan.mapper.LeavemsgMapper;
+import lixianyuan.mapper.custom.LeavemsgCustomMapper;
+
+@Service
+public class LeaveMsgServiceImpl {
+
+	@Autowired
+	private LeavemsgMapper leavemsgMapper;
+
+	@Autowired
+	private LeavemsgCustomMapper leavemsgCustomMapper;
+
+	/**
+	 * 增加留言
+	 * 
+	 * @param leaveMsg
+	 * @return
+	 */
+	public int addLeaveMsg(Leavemsg leaveMsg) {
+		int result = leavemsgMapper.insert(leaveMsg);
+		return result > 0 ? result : 0;
+	}
+
+	/**
+	 * 修改留言
+	 * 
+	 * @param leaveMsg
+	 * @return
+	 */
+	public int updateLeaveMsg(Leavemsg leaveMsg) {
+		int result = leavemsgMapper.updateByPrimaryKeySelective(leaveMsg);
+		return result > 0 ? result : 0;
+	}
+
+	/**
+	 * 删除留言
+	 * 
+	 * @param leaveMsgId
+	 * @return
+	 */
+	public int delLeaveMsgById(String leaveMsgId) {
+		int result = leavemsgMapper.deleteByPrimaryKey(leaveMsgId);
+		return result > 0 ? result : 0;
+	}
+
+	/**
+	 * 根据条件查询出符合条件的留言
+	 * 
+	 * @param map
+	 *            查询条件 包括当前页页号currentPage和每页显示的记录数currentCount
+	 * @return 总记录数
+	 */
+	public List<Leavemsg> findleaveMsgByCondition(Map<String, Object> map) {
+		List<Leavemsg> leavemsgList = leavemsgCustomMapper.findleaveMsgByCondition(map);
+		return leavemsgList.size() > 0 ? leavemsgList : null;
+	}
+
+	/**
+	 * 根据条件查询出符合条件的总记录数
+	 * 
+	 * @param map
+	 *            查询条件
+	 * @return 总记录数
+	 */
+	public int findleaveMsgCountByCondition(Map<String, Object> map) {
+		int result = leavemsgCustomMapper.findleaveMsgCountByCondition(map);
+		return result > 0 ? result : 0;
+	}
+
+	/**
+	 * 查询出所有的留言及其对应的回复留言 每一个map代表mysql中的一行数据
+	 * 
+	 * @return 留言及其对应的回复留言
+	 */
+	public List<Map<String, Object>> findleaveMsgAndReply() {
+		List<Map<String, Object>> listMap = leavemsgCustomMapper.findleaveMsgAndReply();
+		return listMap.size() > 0 ? listMap : null;
+	}
+
+}
---
+++ lixianyuan/src/main/java/lixianyuan/service/leavemsgandreply/impl/ReplyServiceImpl.java
@@ -0,0 +1,78 @@
+package lixianyuan.service.leavemsgandreply.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lixianyuan.bean.Reply;
+import lixianyuan.mapper.ReplyMapper;
+import lixianyuan.mapper.custom.ReplyCustomMapper;
+
+@Service
+public class ReplyServiceImpl {
+	@Autowired
+	private ReplyMapper replyMapper;
+
+	@Autowired
+	private ReplyCustomMapper replyCustomMapper;
+
+	/**
+	 * 增加回复留言，必须要有一个已知的对应的留言id-》leavemsgid
+	 * 
+	 * @param reply
+	 * @return
+	 */
+	public int addReply(Reply reply) {
+		int result = replyMapper.insert(reply);
+		return result > 0 ? result : 0;
+	}
+
+	/**
+	 * 修改回复留言
+	 * 
+	 * @param reply
+	 * @return
+	 */
+	public int updateReply(Reply reply) {
+		int result = replyMapper.updateByPrimaryKeySelective(reply);
+		return result > 0 ? result : 0;
+	}
+
+	/**
+	 * 删除回复留言
+	 * 
+	 * @param replyId
+	 * @return
+	 */
+	public int delReplyByReplyId(String replyId) {
+		int result = replyMapper.deleteByPrimaryKey(replyId);
+		return result > 0 ? result : 0;
+	}
+
+	/**
+	 * 根据条件查询符合的回复留言
+	 * 
+	 * @param map
+	 *            查询条件 包括当前页页号和每页显示的总记录数
+	 * @return 符合条件的回复留言
+	 */
+	public List<Reply> findAllReplyByCondition(Map<String, Object> map) {
+		List<Reply> replyList = replyCustomMapper.findAllReplyByCondition(map);
+		return replyList.size() > 0 ? replyList : null;
+	}
+
+	/**
+	 * 根据条件查询符合的回复留言总条数
+	 * 
+	 * @param map
+	 *            查询条件 包括当前页页号和每页显示的总记录数
+	 * @return 总记录数
+	 */
+	public int findAllReplyCountByCondition(Map<String, Object> map) {
+		int result = replyCustomMapper.findAllReplyCountByCondition(map);
+		return result > 0 ? result : 0;
+	}
+
+}
